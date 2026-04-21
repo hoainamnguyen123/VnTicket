8f173b798509ffade9a21e349ade22f640f80c2a
hoainamnguyen123 <namtlhb68@gmail.com>
2026-04-21 11:12:11 +0700

    fix(backend): Set JVM TimeZone to GMT+7 to fix VNPay Code 15 and Frontend Timezone Desync

--- vnticket-backend/src/main/java/com/vnticket/VnTicketApplication.java
+++ vnticket-backend/src/main/java/com/vnticket/VnTicketApplication.java
@@ -5,10 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 public class VnTicketApplication {
+
+    @PostConstruct
+    public void init() {
+        // Cấu hình múi giờ chuẩn của ứng dụng là giờ Việt Nam
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VnTicketApplication.class, args);
